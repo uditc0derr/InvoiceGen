149b7b388bf8ac282efb40144965fc613c56677c
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-03-29 11:03:34 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://invoicegen-backend.onrender.com/api/auth/login", {
         email,
         password,
       });
