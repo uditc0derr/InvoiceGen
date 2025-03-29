9bfcf5f9fe63a5a16336db015597f4729c3cd539
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-03-29 11:09:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());  
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
   }));
