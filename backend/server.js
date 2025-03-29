d78059c4103504eb00704447feab1cc865fb5db7
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-03-29 11:14:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());  
 app.use(cors({
-    origin: "*",
+    origin: "https://invoicegenfrontend.onrender.com",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
   }));
